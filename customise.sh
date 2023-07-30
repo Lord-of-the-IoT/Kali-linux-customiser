a13fad5ef28fa164197b3f08cf711cb1375cfb7d
Lord of the IoT <lord_of_the_iot@proton.me>
2023-07-30 13:12:09 +0100

    Update customise.sh

--- customise.sh
+++ customise.sh
@@ -34,7 +34,7 @@ echo "# KEYBOARD CONFIGURATION FILE\n# Consult the keyboard(5) manual page.\nXKB
 #update and upgrade apt
 apt-get update && apt upgrade
 #downloads packages from apt and pip
-apt-get install git silenttrinity cyberchef gobuster armitage
+apt-get install git silenttrinity cyberchef gobuster armitage seclists
 pip install PyCryptodome pwntools scapy git-dumper
 
 #install atom
