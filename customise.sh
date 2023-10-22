9bfe9435483a66469597f31df3204d0a360a9385
Lord of the IoT <lord_of_the_iot@proton.me>
2023-10-22 21:01:44 +0100

    Update customise.sh
    added sagemath

--- customise.sh
+++ customise.sh
@@ -35,6 +35,21 @@ cd ~/tools
 git clone https://github.com/Sybil-Scan/imagemagick-lfi-poc.git
 git clone https://github.com/ly4k/PwnKit.git
 git clone https://github.com/carlospolop/PEASS-ng.git
+#install sagemath for cryptograpy
+sudo apt install bc binutils bzip2 ca-certificates cliquer cmake curl ecl eclib-tools fflas-ffpack flintqs g++ gengetopt gfan gfortran
+        git glpk-utils gmp-ecm lcalc libatomic-ops-dev libboost-dev libbraiding-dev libbz2-dev libcdd-dev libcdd-tools libcliquer-dev
+        libcurl4-openssl-dev libec-dev libecm-dev libffi-dev libflint-arb-dev libflint-dev libfreetype6-dev libgc-dev libgd-dev libgf2x-dev
+        libgiac-dev libgivaro-dev libglpk-dev libgmp-dev libgsl-dev libhomfly-dev libiml-dev liblfunction-dev liblrcalc-dev liblzma-dev
+        libm4rie-dev libmpc-dev libmpfi-dev libmpfr-dev libncurses5-dev libntl-dev libopenblas-dev libpari-dev libpcre3-dev libplanarity-dev
+        libppl-dev libprimesieve-dev libpython3-dev libqhull-dev libreadline-dev librw-dev libsingular4-dev libsqlite3-dev libssl-dev
+        libsuitesparse-dev libsymmetrica2-dev libz-dev libzmq3-dev libzn-poly-dev m4 make nauty openssl palp pari-doc pari-elldata pari-galdata
+        pari-galpol pari-gp2c pari-seadata patch perl pkg-config planarity ppl-dev python3-distutils python3-venv r-base-dev r-cran-lattice singular
+        sqlite3 sympow tachyon tar tox xcas xz-utils
+git clone --branch master https://github.com/sagemath/sage.git
+cd sage
+make configure
+./configure
+MAKE="make -j8" make
 
 #install atom
 apt-get install git libasound2 libcurl4 libgbm1 libgcrypt20 libgtk-3-0 libnotify4 libnss3 libglib2.0-bin xdg-utils libx11-xcb1 libxcb-dri3-0 libxss1 libxtst6 libxkbfile1 #prequisites
